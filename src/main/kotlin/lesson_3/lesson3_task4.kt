4ff54068fb1959b10d29ea2871c3ef1f58253f8a
TataPuH-90 <abzalov-rifat@mail.ru>
2025-04-01 20:28:46 +0300

    Ks-3-4 chess

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var where = "E2"
+    var outWhere = "E4"
+    var moveNumber = 1
+
+    println(" $where - $outWhere ; $moveNumber")
+
+    where = "D2"
+    outWhere = "D3"
+    moveNumber = 2
+
+    println(" $where - $outWhere ; $moveNumber")
+}

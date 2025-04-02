b7983839492f34608390618f46720b00da1cb7aa
TataPuH-90 <abzalov-rifat@mail.ru>
2025-04-02 19:57:23 +0300

    Ks-4-2 fix format

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -5,14 +5,18 @@ fun main() {
     val cargoWeight1 = 20
     val cargoVolume1 = 80
 
-    println("Груз с весом 20 кг и обьемом 80 л соответсвует категории 'Average' : ${MINIMUM_WEIGHT < cargoWeight1 && cargoWeight1  <= MAXIMUM_WEIGHT && cargoVolume1 < VOLUME }")
+    println("Груз с весом 20 кг и обьемом 80 л соответсвует категории 'Average' : " +
+            "${MINIMUM_WEIGHT < cargoWeight1 && cargoWeight1 <= MAXIMUM_WEIGHT 
+                    && cargoVolume1 < VOLUME}")
 
     val cargoWeight2 = 50
     val cargoVolume2 = 100
 
-    println("Груз с весом 50 кг и обьемом 100 л соответсвует категории 'Average' : ${MINIMUM_WEIGHT < cargoWeight2 && cargoWeight2  <= MAXIMUM_WEIGHT && cargoVolume2 < VOLUME }")
+    println("Груз с весом 50 кг и обьемом 100 л соответсвует категории 'Average' : " +
+            "${MINIMUM_WEIGHT < cargoWeight2 && cargoWeight2 <= MAXIMUM_WEIGHT
+                    && cargoVolume2 < VOLUME}")
 }
 
-  const val MINIMUM_WEIGHT = 35
-  const val MAXIMUM_WEIGHT= 100
-  const val VOLUME = 100
+const val MINIMUM_WEIGHT = 35
+const val MAXIMUM_WEIGHT = 100
+const val VOLUME = 100

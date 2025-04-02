10475d871ac7f77de087452326030497161b9af9
TataPuH-90 <abzalov-rifat@mail.ru>
2025-04-02 19:17:31 +0300

    Ks-4-3 agro

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -2,12 +2,17 @@ package org.example.lesson_4
 
 fun main() {
 
-    val weatherToday : Boolean = true
-    val awning : Boolean =true
-    val airHumidity : Int = 20
-    val timeOfYear : String = "зима"
+    val isSunnyToday: Boolean = true
+    val isAwningOpen: Boolean = true
+    val airHumidity: Int = 20
+    val timeOfYear: String = "зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(weatherToday == WEATHER) && (awning == OPEN_AWNING) && (timeOfYear == Year_TIME) && (airHumidity== AIR)}")
+    println("Благоприятные ли условия сейчас для роста бобовых?" +
+            " ${(isSunnyToday == WEATHER_IS_SUNNY) && (isAwningOpen == OPEN_AWNING)
+            && (timeOfYear != Year_TIME) && (airHumidity == AIR)}")
 }
-  const val WEATHER = true ; const val Year_TIME = "не зима"
-  const val OPEN_AWNING = true ; const val AIR = 20
+
+const val WEATHER_IS_SUNNY = true
+const val Year_TIME = "зима"
+const val OPEN_AWNING = true
+const val AIR = 20

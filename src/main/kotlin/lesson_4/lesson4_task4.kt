6eed5e9b00e901fc905e73c649b1116e33086055
TataPuH-90 <abzalov-rifat@mail.ru>
2025-04-03 19:09:08 +0300

    KS-4-4 transcoding

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,13 +2,17 @@ package org.example.lesson_4
 
 fun main() {
 
-    val monthlyPlanArmsLegs = 1..31 step 2
+    val month = 31
+    val monthlyPlanArmsLegs = month % THE_PARITY_DETERMINANT
     val trainingDAy = 5
+    val eventOfTrainingDay = trainingDAy % THE_PARITY_DETERMINANT
+
     println(
-        """|Упражнение для рук: ${trainingDAy in monthlyPlanArmsLegs} 
-            |Упражнение для ног: ${trainingDAy !in monthlyPlanArmsLegs} 
-            |Упражнение для спины: ${trainingDAy !in monthlyPlanArmsLegs} 
-            |Упражнение для пресса: ${trainingDAy in monthlyPlanArmsLegs} """.trimMargin()
+        """|Упражнение для рук: ${eventOfTrainingDay == monthlyPlanArmsLegs}
+          |Упражнение для ног: ${eventOfTrainingDay != monthlyPlanArmsLegs}
+          |Упражнение для спины: ${eventOfTrainingDay != monthlyPlanArmsLegs}
+          |Упражнение для пресса: ${eventOfTrainingDay == monthlyPlanArmsLegs} """.trimMargin()
     )
 }
 
+const val THE_PARITY_DETERMINANT = 2
